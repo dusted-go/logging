1590ec59e5e310694c486916c9c9887d98f08469
Viktor Alenkov <viktor@alenkov.name>
2024-07-30 10:16:03 +0300

    #7 An empty body is output as `{}`
    1. Added option to disable empty Attrs output
    2. Added a minimal [editorconfig](https://editorconfig.org/) so that the IDE uses uniform project settings

--- prettylog/prettylog_test.go
+++ prettylog/prettylog_test.go
@@ -18,7 +18,7 @@ func (cs *captureStream) Write(bytes []byte) (int, error) {
 
 func Test_WritesToProvidedStream(t *testing.T) {
 	cs := &captureStream{}
-	handler := New(nil, WithDestinationWriter(cs))
+	handler := New(nil, WithDestinationWriter(cs), WithOutputEmptyAttrs())
 	logger := slog.New(handler)
 
 	logger.Info("testing logger")
@@ -27,6 +27,26 @@ func Test_WritesToProvidedStream(t *testing.T) {
 	}
 
 	lineMatcher := regexp.MustCompile(`\[\d{2}:\d{2}:\d{2}\.\d{3}\] INFO: testing logger {}`)
+	line := string(cs.lines[0])
+	if lineMatcher.MatchString(line) == false {
+		t.Errorf("expected `testing logger` but found `%s`", line)
+	}
+	if !strings.HasSuffix(line, "\n") {
+		t.Errorf("exected line to be terminated with `\\n` but found `%s`", line[len(line)-1:])
+	}
+}
+
+func Test_SkipEmptyAttributes(t *testing.T) {
+	cs := &captureStream{}
+	handler := New(nil, WithDestinationWriter(cs))
+	logger := slog.New(handler)
+
+	logger.Info("testing logger")
+	if len(cs.lines) != 1 {
+		t.Errorf("expected 1 lines logged, got: %d", len(cs.lines))
+	}
+
+	lineMatcher := regexp.MustCompile(`\[\d{2}:\d{2}:\d{2}\.\d{3}\] INFO: testing logger`)
 	line := string(cs.lines[0])
 	if lineMatcher.MatchString(line) == false {
 		t.Errorf("expected `testing logger` but found `%s`", line)
